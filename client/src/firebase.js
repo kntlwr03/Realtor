e44ac7a64cba56fe582fb38ce7087abf53a4cd86
kntlwr03 <115495110+kntlwr03@users.noreply.github.com>
2023-12-08 19:00:16 +0530

    Mern Project 1

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqj7TWB3Z9ia30TqB28mzmguGw-MuFnfc",
+  authDomain: "realtor-95a2b.firebaseapp.com",
+  projectId: "realtor-95a2b",
+  storageBucket: "realtor-95a2b.appspot.com",
+  messagingSenderId: "38496632887",
+  appId: "1:38496632887:web:664d503b71d6027da73fb2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
